a029a9350cd0c193ccabaf8157243a62a79c9739
kamalakars04 <kamalakars04@gmail.com>
2020-09-30 15:40:18 +0530

    [kamalakar].Adding UC1_AbilityToAddContact

--- Program.cs
+++ Program.cs
@@ -2,11 +2,30 @@
 
 namespace AddressBookSystem
 {
+    /// <summary>
+    /// Interface creted for abstraction and easy access for user
+    /// </summary>
+    public interface IAddressBook
+    {
+        void AddContact(string firstName, string lastName, string address, string city, string state, string zip, string phonenumber, string email);
+        void DisplayPhoneNumber(string Name);
+
+    }
     class Program
     {
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program");
+
+            AddressBook addressBook = new AddressBook();
+
+            // Adding contact into address book
+            addressBook.AddContact("first", "Last", "Street NO: 875", "NewYork", "America", "123456", "1234567891", "sample@example.com");
+
+            // Searching phone Number by contact name
+            addressBook.DisplayPhoneNumber("first");
+
         }
     }
 }
