6491e6f3941a34b103ae08f5373e8d308aba1746
kamalakars04 <kamalakars04@gmail.com>
2020-09-30 15:59:35 +0530

    [Kamalakar].[Refactor].Adding UC2_AddingContactThroughConsole

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace AddressBookSystem
     /// </summary>
     public interface IAddressBook
     {
-        void AddContact(string firstName, string lastName, string address, string city, string state, string zip, string phonenumber, string email);
+        void AddContact(string firstName, string lastName, string address, string city, string state, string zip, string phoneNumber, string email);
         void DisplayPhoneNumber(string Name);
 
     }
@@ -16,15 +16,48 @@ namespace AddressBookSystem
         
         static void Main(string[] args)
         {
+            string firstName;
+            string lastName;
+            string address;
+            string city;
+            string state;
+            string zip;
+            string phoneNumber;
+            string email;
             Console.WriteLine("Welcome to Address Book Program");
 
             AddressBook addressBook = new AddressBook();
 
+            Console.WriteLine("Enter the first name of contact");
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter the last name of contact");
+            lastName = Console.ReadLine();
+
+            Console.WriteLine("Enter the address of contact");
+            address = Console.ReadLine();
+
+            Console.WriteLine("Enter the city name of contact");
+            city = Console.ReadLine();
+
+            Console.WriteLine("Enter the state name of contact");
+            state = Console.ReadLine();
+
+            Console.WriteLine("Enter the zip of locality of contact");
+            zip = Console.ReadLine();
+
+            Console.WriteLine("Enter the phone number of contact");
+            phoneNumber = Console.ReadLine();
+
+            Console.WriteLine("Enter the email of contact");
+            email = Console.ReadLine();
+
             // Adding contact into address book
-            addressBook.AddContact("first", "Last", "Street NO: 875", "NewYork", "America", "123456", "1234567891", "sample@example.com");
+            addressBook.AddContact(firstName,lastName,address,city,state,zip,phoneNumber,email);
 
             // Searching phone Number by contact name
-            addressBook.DisplayPhoneNumber("first");
+            Console.WriteLine("Enter the name of candidate to get Phone number");
+            addressBook.DisplayPhoneNumber(Console.ReadLine());
 
         }
     }
